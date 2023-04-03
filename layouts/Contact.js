540a24184863101ec1f4553f4d1be1dfb70e2ad1
Jiten <jitenvaidya@gmail.com>
2023-04-03 13:44:48 +0530

    auto hight

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -15,8 +15,10 @@ const Contact = ({ data }) => {
             
           <iframe 
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.015906213647!2d73.14208701487742!3d22.315238185315817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc9f063c9c445%3A0xd4cd501246d5d840!2sVihav%20Supremus!5e0!3m2!1sen!2sin!4v1680507199285!5m2!1sen!2sin" 
-      width="600" 
-      height="450" 
+      width="auto" 
+      height="auto" 
+      style={{ border: 0 }}
+      allowFullScreen={true}
       
     />
 
