13634c3057c590430810f5cf05a34c9a68200e38
Jiten <jitenvaidya@gmail.com>
2023-04-03 13:49:17 +0530

    chking

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -11,7 +11,7 @@ const Contact = ({ data }) => {
       <div className="container">
         {markdownify(title, "h1", "text-center font-normal")}
         <div className="section row pb-0">
-          <div className="col-12 md:col-6 lg:col-7">
+          <div className="col-12 md:col-10 lg:col-7">
             
           <iframe 
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.015906213647!2d73.14208701487742!3d22.315238185315817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc9f063c9c445%3A0xd4cd501246d5d840!2sVihav%20Supremus!5e0!3m2!1sen!2sin!4v1680507199285!5m2!1sen!2sin" 
@@ -23,7 +23,7 @@ const Contact = ({ data }) => {
     />
 
           </div>
-          <div className="content col-12 md:col-6 lg:col-5">
+          <div className="content col-12 md:col-2 lg:col-5">
             {markdownify(info.title, "h4")}
             {markdownify(info.description, "p", "mt-4")}
             <ul className="contact-list mt-5">
