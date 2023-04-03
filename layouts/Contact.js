e33883a4eb9ce3b655aa62d02f35082c9f804b3c
Jiten <jitenvaidya@gmail.com>
2023-04-03 14:42:56 +0530

    one  more

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -11,19 +11,16 @@ const Contact = ({ data }) => {
       <div className="container">
         {markdownify(title, "h1", "text-center font-normal")}
         <div className="section row pb-0">
-          <div className="col-12 md:col-10 lg:col-7">
-            
+          <div className="col-12 md:col-6 lg:col-7 bg-theme-light ">
+            <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh iiuiohiuhiuh uhiuhi huiuhiuihuhi uhiu ibbnbvbvhf fu uyguy uygubjht fytfyv</p>
           <iframe 
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.015906213647!2d73.14208701487742!3d22.315238185315817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc9f063c9c445%3A0xd4cd501246d5d840!2sVihav%20Supremus!5e0!3m2!1sen!2sin!4v1680507199285!5m2!1sen!2sin" 
-      width="auto" 
-      height="auto" 
-      style={{ border: 0 }}
-      allowFullScreen={true}
+      
       
     />
 
           </div>
-          <div className="content col-12 md:col-2 lg:col-5">
+          <div className="content col-12 md:col-6 lg:col-5">
             {markdownify(info.title, "h4")}
             {markdownify(info.description, "p", "mt-4")}
             <ul className="contact-list mt-5">
