c87d001d62421cd7d2351a77a1e366add4a64696
Jiten <jitenvaidya@gmail.com>
2023-04-03 14:54:05 +0530

    100%

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -11,11 +11,12 @@ const Contact = ({ data }) => {
       <div className="container">
         {markdownify(title, "h1", "text-center font-normal")}
         <div className="section row pb-0">
-          <div className="col-12 md:col-6 lg:col-7 bg-theme-light ">
-            <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh iiuiohiuhiuh uhiuhi huiuhiuihuhi uhiu ibbnbvbvhf fu uyguy uygubjht fytfyv</p>
+          <div className="col-12 md:col-6 lg:col-7">
+            
           <iframe 
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.015906213647!2d73.14208701487742!3d22.315238185315817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc9f063c9c445%3A0xd4cd501246d5d840!2sVihav%20Supremus!5e0!3m2!1sen!2sin!4v1680507199285!5m2!1sen!2sin" 
-      
+      width="100%" 
+      height="100%" 
       
     />
 
