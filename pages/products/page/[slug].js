f15bbe3847e2201dae97705a7c368cc18b628b03
Jiten <jitenvaidya@gmail.com>
2023-04-05 16:38:51 +0530

    content

--- pages/products/page/[slug].js
+++ pages/products/page/[slug].js
@@ -22,7 +22,7 @@ const BlogPagination = ({ postIndex, posts, currentPage, pagination }) => {
       <section className="section">
         <div className="container">
           {markdownify(title, "h1", "h1 text-center font-normal text-[56px]")}
-          {markdownify(subtitle, "h3", "h3 mb-6 mt-6 text-left")}
+          {markdownify(subtitle, "h4", "h4 mb-6 mt-6 text-left")}
           <Posts posts={currentPosts} />
           <Pagination
             section={blog_folder}
